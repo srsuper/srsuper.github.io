418d958d4e11dc17ae367be8f4432b1cd39d5515
srsuper <aot5310@gmail.com>
2020-04-29 05:46:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,48 @@
 <?php
+   $accessToken = "TzEp9W6r6st4xaXK0J5IOpWpDgcV0IcRqIqshNK4Vl0bidlgX0OYe7PjENOgk6A7sLmTRp7ZWbUjkgw9umOBKcpjjzCu/REJCERZbvGoAfWuHFVxOI0v4pG0vSFGfAD6t4zO0GpF1FLL+jAxLl8wbwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+$content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+$arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+//รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+//รับ id ว่ามาจากไหน
+   if(isset($arrayJson['events'][0]['source']['userId']){
+      $id = $arrayJson['events'][0]['source']['userId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['groupId'])){
+      $id = $arrayJson['events'][0]['source']['groupId'];
+   }
+   else if(isset($arrayJson['events'][0]['source']['room'])){
+      $id = $arrayJson['events'][0]['source']['room'];
+   }
+#ตัวอย่าง Message Type "Text + Sticker"
+   if($message == "สวัสดี"){
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   }
+function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+$ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+exit;
+?>
+<?php
 // กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
