f693c33288215bab8d8b96cf74a6087edc5a5076
srsuper <aot5310@gmail.com>
2020-05-04 06:18:19 +0700

    Update limit1.php

--- limit1.php
+++ limit1.php
@@ -6,5 +6,5 @@ $token = 'ocAFOiVlI3yRmol8tfs3bwsS4t6bORh8bRXoUVenleS';
 $ln = new KS\Line\LineNotify($token);
 
 $text = 'ตัวอื่นก็ แทงได้นะจ๊ะ หลังป้ายนี้ แทง 1 ไม่ติด ทุกกรณี '; // Line Notify บังคับให้ใส่ข้อความ แต่อยากส่งแแต่รูปภาพเลยใส่ space ไว้
-$image_path = '/tmp/limit1.jpg'; //Line notify allow only jpeg and png file
+$image_path = '/img/limit1.jpg'; //Line notify allow only jpeg and png file
 $ln->send($text, $image_path);
