831a25b2fa26f8740cf25570b9bfa617de968344
srsuper <aot5310@gmail.com>
2020-05-04 22:33:49 +0700

    Add files via upload

---
+++ js/CLang.js
@@ -0,0 +1,20 @@
+TEXT_MONEY     = "ยอดเงิน";
+TEXT_PLAY      = "เล่น";
+TEXT_BET       = "เดิมพัน";
+TEXT_COIN      = "เหรียญ";
+TEXT_MAX_BET   = "เดิมพัน สูงสุด";
+TEXT_INFO      = "INFO";
+TEXT_LINES     = "LINES";
+TEXT_SPIN      = "หมุน";
+TEXT_WIN       = "ชนะ";
+TEXT_HELP_WILD = "THIS SIMBOL IS A JOLLY WHICH CAN REPLACE ANY OTHER SYMBOL TO MAKE UP A COMBO";
+TEXT_CREDITS_DEVELOPED = "DEVELOPED BY";
+TEXT_CURRENCY = "฿";
+TEXT_PRELOADER_CONTINUE = "เริ่มเกมส์";
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "เยี่ยม!!!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "คะแนน ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
